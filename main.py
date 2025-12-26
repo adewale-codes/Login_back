9ef60e49e28e2fba2d43a3d126ccb85bcddae35b
adewale-codes <adewaleabiola78@gmail.com>
2025-12-26 21:41:38 +0000

    added deployment url to CORS

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = FastAPI(title="3-Level Image Password Auth API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://ade-login.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
